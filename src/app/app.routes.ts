634f78735122d58168d6517a01c17e14c06845f9
Sebastian <sebasisaza_27@hotmail.com>
2025-07-29 12:20:17 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: HomeComponent },
+    // { path: 'detail', component: DetailComponent },
+];
